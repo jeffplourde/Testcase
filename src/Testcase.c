cfe448e1c62e5cd4fbdd203a1a6ceac425e46385
Jeff Plourde <jeff@mdpnp.org>
2015-04-25 10:34:44 -0400

    Cleans up and proves that it's working by writing a sample.

--- src/Testcase.c
+++ src/Testcase.c
@@ -4,174 +4,62 @@
 
 #define MAX_STRING_SIZE         1024
 
-static void on_data_available_callback(void*, DDS_DataReader*);
+static void on_data_available_callback1(void*, DDS_DataReader*);
+static void on_data_available_callback2(void*, DDS_DataReader*);
+static void on_data_available_callback3(void*, DDS_DataReader*);
 
 DDS_Boolean shutdown_flag = DDS_BOOLEAN_FALSE;
 
+struct Entry {
+    DDS_DomainParticipant *part;
+    DDS_Topic             *t;
+    DDS_Publisher         *p;
+    DDS_Subscriber        *s;
+    DDS_DataReader        *r;
+    DDS_DataWriter        *w;
+    DDS_StringDataWriter  *sw;
+    struct DDS_DataReaderListener listener;
+};
+
 int main() {
-    DDS_DomainParticipant *       part1 = NULL;
-    DDS_DomainParticipant *       part2 = NULL;
-    DDS_DomainParticipant *       part3 = NULL;
-    DDS_Topic *                   t1 = NULL;
-    DDS_Topic *                   t2 = NULL;
-    DDS_Topic *                   t3 = NULL;
-    DDS_Publisher *               p1 = NULL;
-    DDS_Publisher *               p2 = NULL; 
-    DDS_Publisher *               p3 = NULL;
-    DDS_Subscriber *              s1 = NULL;
-    DDS_Subscriber *              s2 = NULL;
-    DDS_Subscriber *              s3 = NULL;
-    DDS_DataReader *              r1 = NULL;
-    DDS_DataReader *              r2 = NULL;
-    DDS_DataReader *              r3 = NULL;
-    DDS_DataWriter *              w1 = NULL;
-    DDS_DataWriter *              w2 = NULL;
-    DDS_DataWriter *              w3 = NULL;
+    struct Entry entries[3];
+    char * start_partition[3] = {"A", "A", "*"};
+    struct DDS_DataReaderListener listeners[3] = { DDS_DataReaderListener_INITIALIZER, DDS_DataReaderListener_INITIALIZER, DDS_DataReaderListener_INITIALIZER };
+    listeners[0].on_data_available = on_data_available_callback1;
+    listeners[1].on_data_available = on_data_available_callback2;
+    listeners[2].on_data_available = on_data_available_callback3;
 
-    struct DDS_DataReaderListener listener = DDS_DataReaderListener_INITIALIZER;
-    DDS_ReturnCode_t              retcode;
-    int                           main_result = 1; /* error by default */
+    int entry_count = sizeof(entries) / sizeof(struct Entry);
+    for(int i = 0; i < entry_count; i++) {
+        struct DDS_DomainParticipantQos partQos = DDS_DomainParticipantQos_INITIALIZER;
+        DDS_DomainParticipantFactory_get_default_participant_qos(DDS_TheParticipantFactory, &partQos);
+        partQos.transport_builtin.mask = DDS_TRANSPORTBUILTIN_UDPv4;
 
-    part1 = DDS_DomainParticipantFactory_create_participant(
-                        DDS_TheParticipantFactory,
-                        0,                              /* Domain ID */
-                        &DDS_PARTICIPANT_QOS_DEFAULT,   /* QoS */
-                        NULL,                           /* Listener */
-                        DDS_STATUS_MASK_NONE);
-    if (part1 == NULL) {
-        puts("Unable to create domain participant 1.");
-        goto clean_exit;
+        entries[i].part = DDS_DomainParticipantFactory_create_participant(
+                            DDS_TheParticipantFactory, 0,
+                            &partQos,
+                            NULL, DDS_STATUS_MASK_NONE);
+        entries[i].t = DDS_DomainParticipant_create_topic(
+                        entries[i].part, "Hello, World", DDS_StringTypeSupport_get_type_name(), 
+                        &DDS_TOPIC_QOS_DEFAULT, NULL, DDS_STATUS_MASK_NONE);
+
+        struct DDS_SubscriberQos sQos = DDS_SubscriberQos_INITIALIZER;
+        DDS_DomainParticipant_get_default_subscriber_qos(entries[i].part, &sQos);
+        DDS_StringSeq_ensure_length(&sQos.partition.name, 1, 1);
+        *DDS_StringSeq_get_reference(&sQos.partition.name, 0) = DDS_String_dup(start_partition[i]);
+        entries[i].s = DDS_DomainParticipant_create_subscriber(entries[i].part, &sQos, NULL, DDS_STATUS_MASK_NONE);
+
+        struct DDS_PublisherQos pQos = DDS_PublisherQos_INITIALIZER;
+        DDS_DomainParticipant_get_default_publisher_qos(entries[i].part, &pQos);
+        DDS_StringSeq_ensure_length(&pQos.partition.name, 1, 1);
+        *DDS_StringSeq_get_reference(&pQos.partition.name, 0) = DDS_String_dup(start_partition[i]);
+        entries[i].p = DDS_DomainParticipant_create_publisher(entries[i].part, &pQos, NULL, DDS_STATUS_MASK_NONE);
+
+        entries[i].r = DDS_Subscriber_create_datareader(entries[i].s, DDS_Topic_as_topicdescription(entries[i].t), &DDS_DATAREADER_QOS_DEFAULT, &listeners[i], DDS_DATA_AVAILABLE_STATUS);
+        entries[i].w = DDS_Publisher_create_datawriter(entries[i].p, entries[i].t, &DDS_DATAWRITER_QOS_DEFAULT, NULL, DDS_STATUS_MASK_NONE);
+        entries[i].sw = DDS_StringDataWriter_narrow(entries[i].w);
     }
 
-    part2 = DDS_DomainParticipantFactory_create_participant(
-                        DDS_TheParticipantFactory,
-                        0,                              /* Domain ID */
-                        &DDS_PARTICIPANT_QOS_DEFAULT,   /* QoS */
-                        NULL,                           /* Listener */
-                        DDS_STATUS_MASK_NONE);
-    if (part2 == NULL) {
-        puts("Unable to create domain participant 2.");
-        goto clean_exit;
-    }    
-
-    part3 = DDS_DomainParticipantFactory_create_participant(
-                        DDS_TheParticipantFactory,
-                        0,                              /* Domain ID */
-                        &DDS_PARTICIPANT_QOS_DEFAULT,   /* QoS */
-                        NULL,                           /* Listener */
-                        DDS_STATUS_MASK_NONE);
-    if (part3 == NULL) {
-        puts("Unable to create domain participant 3.");
-        goto clean_exit;
-    }    
-
-    t1 = DDS_DomainParticipant_create_topic(
-                        part1, 
-                        "Hello, World",                        /* Topic name*/
-                        DDS_StringTypeSupport_get_type_name(), /* Type name */
-                        &DDS_TOPIC_QOS_DEFAULT,                /* Topic QoS */
-                        NULL,                                  /* Listener  */
-                        DDS_STATUS_MASK_NONE);
-    if (t1 == NULL) {
-        puts("Unable to create topic1.");
-        goto clean_exit;
-    }
-
-    t2 = DDS_DomainParticipant_create_topic(
-                        part2, 
-                        "Hello, World",                        /* Topic name*/
-                        DDS_StringTypeSupport_get_type_name(), /* Type name */
-                        &DDS_TOPIC_QOS_DEFAULT,                /* Topic QoS */
-                        NULL,                                  /* Listener  */
-                        DDS_STATUS_MASK_NONE);
-    if (t2 == NULL) {
-        puts("Unable to create topic2.");
-        goto clean_exit;
-    }    
-
-    t3 = DDS_DomainParticipant_create_topic(
-                        part3, 
-                        "Hello, World",                        /* Topic name*/
-                        DDS_StringTypeSupport_get_type_name(), /* Type name */
-                        &DDS_TOPIC_QOS_DEFAULT,                /* Topic QoS */
-                        NULL,                                  /* Listener  */
-                        DDS_STATUS_MASK_NONE);
-    if (t3 == NULL) {
-        puts("Unable to create topic3.");
-        goto clean_exit;
-    }    
-    struct DDS_SubscriberQos sQos;
-    DDS_DomainParticipant_get_default_subscriber_qos(part1, &sQos);
-    DDS_StringSeq_ensure_length(&sQos.partition.name, 1, 1);
-    *DDS_StringSeq_get_reference(&sQos.partition.name, 0) = DDS_String_dup("A");
-
-    s1 = DDS_DomainParticipant_create_subscriber(part1, &sQos, NULL, DDS_STATUS_MASK_NONE);
-    if(!s1) {
-        puts("Unable to create s1");
-        goto clean_exit;
-    }
-    DDS_DomainParticipant_get_default_subscriber_qos(part2, &sQos);
-    DDS_StringSeq_ensure_length(&sQos.partition.name, 1, 1);
-    *DDS_StringSeq_get_reference(&sQos.partition.name, 0) = DDS_String_dup("A");
-    s2 = DDS_DomainParticipant_create_subscriber(part2, &sQos, NULL, DDS_STATUS_MASK_NONE);
-    if(!s2) {
-        puts("Unable to create s2");
-        goto clean_exit;
-    }
-    DDS_DomainParticipant_get_default_subscriber_qos(part3, &sQos);
-    DDS_StringSeq_ensure_length(&sQos.partition.name, 1, 1);
-    *DDS_StringSeq_get_reference(&sQos.partition.name, 0) = DDS_String_dup("*");
-    s3 = DDS_DomainParticipant_create_subscriber(part3, &sQos, NULL, DDS_STATUS_MASK_NONE);
-    if(!s3) {
-        puts("Unable to create s3");
-        goto clean_exit;
-    }   
-    struct DDS_PublisherQos pQos;
-    DDS_DomainParticipant_get_default_publisher_qos(part1, &pQos);
-    DDS_StringSeq_ensure_length(&pQos.partition.name, 1, 1);
-    *DDS_StringSeq_get_reference(&pQos.partition.name, 0) = DDS_String_dup("A");
-    p1 = DDS_DomainParticipant_create_publisher(part1, &pQos, NULL, DDS_STATUS_MASK_NONE);
-    if(!p1) {
-        puts("Unable to create p1");
-        goto clean_exit;
-    }
-    DDS_DomainParticipant_get_default_publisher_qos(part2, &pQos);
-    DDS_StringSeq_ensure_length(&pQos.partition.name, 1, 1);
-    *DDS_StringSeq_get_reference(&pQos.partition.name, 0) = DDS_String_dup("A");    
-    p2 = DDS_DomainParticipant_create_publisher(part2, &pQos, NULL, DDS_STATUS_MASK_NONE);
-    if(!p2) {
-        puts("Unable to create p2");
-        goto clean_exit;
-    }
-    DDS_DomainParticipant_get_default_publisher_qos(part3, &pQos);
-    DDS_StringSeq_ensure_length(&pQos.partition.name, 1, 1);
-    *DDS_StringSeq_get_reference(&pQos.partition.name, 0) = DDS_String_dup("*");
-    p3 = DDS_DomainParticipant_create_publisher(part3, &pQos, NULL, DDS_STATUS_MASK_NONE);
-    if(!p3) {
-        puts("Unable to create p3");
-        goto clean_exit;
-    }    
-
-    r1 = DDS_Subscriber_create_datareader(s1, DDS_Topic_as_topicdescription(t1), &DDS_DATAREADER_QOS_DEFAULT, NULL, DDS_STATUS_MASK_NONE);
-    r2 = DDS_Subscriber_create_datareader(s2, DDS_Topic_as_topicdescription(t2), &DDS_DATAREADER_QOS_DEFAULT, NULL, DDS_STATUS_MASK_NONE);
-    r3 = DDS_Subscriber_create_datareader(s3, DDS_Topic_as_topicdescription(t3), &DDS_DATAREADER_QOS_DEFAULT, NULL, DDS_STATUS_MASK_NONE);
-
-    w1 = DDS_Publisher_create_datawriter(p1, t1, &DDS_DATAWRITER_QOS_DEFAULT, NULL, DDS_STATUS_MASK_NONE);
-    w2 = DDS_Publisher_create_datawriter(p2, t2, &DDS_DATAWRITER_QOS_DEFAULT, NULL, DDS_STATUS_MASK_NONE);
-    w3 = DDS_Publisher_create_datawriter(p3, t3, &DDS_DATAWRITER_QOS_DEFAULT, NULL, DDS_STATUS_MASK_NONE);
-
-    /* Create the data reader using the default publisher */
-    listener.on_data_available = on_data_available_callback;
-    r1 = DDS_Subscriber_create_datareader(
-                        s1,
-                        DDS_Topic_as_topicdescription(t1),
-                        &DDS_DATAREADER_QOS_DEFAULT,    /* QoS */
-                        &listener,                      /* Listener */
-                        DDS_DATA_AVAILABLE_STATUS);
-    if (r1 == NULL) {
-        puts("Unable to create data reader.");
-        goto clean_exit;
-    }
     
     /* --- Sleep During Asynchronous Reception ---------------------------- */
 
@@ -181,6 +69,7 @@ int main() {
     puts("Ready to read data.");
     puts("Press CTRL+C to terminate.");
     for (;;) {
+        DDS_StringDataWriter_write(entries[0].sw, "Hello", &DDS_HANDLE_NIL);
 #ifdef RTI_WIN32
         Sleep(2000);
 #else
@@ -190,59 +79,14 @@ int main() {
             break;
         }
     }
-    
-    /* --- Clean Up ------------------------------------------------------- */
 
-    main_result = 0;
-clean_exit:
     puts("Exiting...");
 
-    if (part1 != NULL) {
-        retcode = DDS_DomainParticipant_delete_contained_entities(
-                        part1);
-        if (retcode != DDS_RETCODE_OK) {
-            puts("Deletion failed.");
-            main_result = 1;
-        }
-        retcode = DDS_DomainParticipantFactory_delete_participant(
-                        DDS_TheParticipantFactory,
-                        part1);
-        if (retcode != DDS_RETCODE_OK) {
-            puts("Deletion failed.");
-            main_result = 1;
-        }
-    } 
-    if (part2 != NULL) {
-        retcode = DDS_DomainParticipant_delete_contained_entities(
-                        part2);
-        if (retcode != DDS_RETCODE_OK) {
-            puts("Deletion failed.");
-            main_result = 1;
-        }
-        retcode = DDS_DomainParticipantFactory_delete_participant(
-                        DDS_TheParticipantFactory,
-                        part2);
-        if (retcode != DDS_RETCODE_OK) {
-            puts("Deletion failed.");
-            main_result = 1;
-        }
-    }     
-    if (part3 != NULL) {
-        retcode = DDS_DomainParticipant_delete_contained_entities(
-                        part3);
-        if (retcode != DDS_RETCODE_OK) {
-            puts("Deletion failed.");
-            main_result = 1;
-        }
-        retcode = DDS_DomainParticipantFactory_delete_participant(
-                        DDS_TheParticipantFactory,
-                        part3);
-        if (retcode != DDS_RETCODE_OK) {
-            puts("Deletion failed.");
-            main_result = 1;
-        }
-    }     
-    return main_result;
+    for(int i = 0; i < entry_count; i++) {
+        DDS_DomainParticipant_delete_contained_entities(entries[i].part);
+        DDS_DomainParticipantFactory_delete_participant(DDS_TheParticipantFactory, entries[i].part);
+    }
+
 }
 
 /* on_data_available_callback
@@ -250,10 +94,11 @@ clean_exit:
  * This is the callback function that is invoked by RTI Connext whenever a new sample is received.
  */
 static void on_data_available_callback(void *listener_data /* unused */, 
-                                       DDS_DataReader* data_reader) {
+                                       DDS_DataReader* data_reader,
+                                       const char * identifier) {
     DDS_StringDataReader *string_reader = NULL;
-    char                  sample[MAX_STRING_SIZE]; 
-    struct DDS_SampleInfo info;
+    struct DDS_StringSeq samples = DDS_SEQUENCE_INITIALIZER;
+    struct DDS_SampleInfoSeq infos = DDS_SEQUENCE_INITIALIZER;
     DDS_ReturnCode_t      retcode;
 
     /* Perform a safe type-cast from a generic data reader into a
@@ -267,27 +112,44 @@ static void on_data_available_callback(void *listener_data /* unused */,
     }
     /* Loop until there are messages available in the queue */
     for(;;) {
-        retcode = DDS_StringDataReader_take_next_sample(
+        retcode = DDS_StringDataReader_read(
                             string_reader,
-                            sample,
-                            &info);
+                            &samples,
+                            &infos,
+                            DDS_LENGTH_UNLIMITED,
+                            DDS_NOT_READ_SAMPLE_STATE,
+                            DDS_ANY_VIEW_STATE,
+                            DDS_ANY_INSTANCE_STATE);
         if (retcode == DDS_RETCODE_NO_DATA) {
-            /* No more samples */
             break;
         } else if (retcode != DDS_RETCODE_OK) {
             printf("Unable to take data from data reader, error %d\n", retcode);
             return;
         }
-        if (info.valid_data) {
-            /* Data is valid (this isn't just a lifecycle sample): print it */
-            puts(sample);
-            /* If empty string is received, clean shutdown*/
-            if(strlen(sample) == 0){
-                shutdown_flag = DDS_BOOLEAN_TRUE;	
-            }
+        DDS_Long length = DDS_SampleInfoSeq_get_length(&infos);
+
+        for(DDS_Long i = 0; i < length; i++) {
+            struct DDS_SampleInfo info = DDS_SampleInfoSeq_get(&infos, i);
+            char * sample = DDS_StringSeq_get(&samples, i);
+            fprintf(stderr, "%s: %s %s\n", identifier, sample, 0!=(info.instance_state&DDS_ALIVE_INSTANCE_STATE)?"ALIVE":"NOT ALIVE");
+            // TODO CHECK FOR LIVELINESS AND REPORT TO CONSOLE
         }
+
+        DDS_StringDataReader_return_loan(string_reader, &samples, &infos);
     }
 }
 
+static void on_data_available_callback1(void *listener_data /* unused */, 
+                                       DDS_DataReader* data_reader) {
+    on_data_available_callback(listener_data, data_reader, "1");
+}
 
+static void on_data_available_callback2(void *listener_data /* unused */, 
+                                       DDS_DataReader* data_reader) {
+    on_data_available_callback(listener_data, data_reader, "2");
+}
 
+static void on_data_available_callback3(void *listener_data /* unused */, 
+                                       DDS_DataReader* data_reader) {
+    on_data_available_callback(listener_data, data_reader, "3");
+}
